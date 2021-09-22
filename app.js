a27d20c14694aa60fa4d18b41fd68009c09040dc
Your Name <you@example.com>
2021-09-22 11:17:58 -0300

    conexion heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 // levantar server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("el servidor funciona en puerto 3000");
 });
 
